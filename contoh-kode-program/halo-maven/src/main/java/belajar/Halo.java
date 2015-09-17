356262862d1cebc392e420eea27098af7ec1c078
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-17 20:55:26 +0700

    contoh project maven sederhana

---
+++ contoh-kode-program/halo-maven/src/main/java/belajar/Halo.java
@@ -0,0 +1,12 @@
+package belajar;
+
+import org.joda.time.DateTime;
+
+public class Halo {
+    public static void main(String[] x){
+        System.out.println("Halo");
+
+        DateTime sekarang = new DateTime();
+        System.out.println("Waktu saat ini : "+sekarang);
+    }
+}
