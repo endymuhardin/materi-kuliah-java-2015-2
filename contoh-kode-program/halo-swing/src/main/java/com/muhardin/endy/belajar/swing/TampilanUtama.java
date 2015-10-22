58259c7d00341dab24bd19d1c592566d9ad80bc4
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-22 19:49:13 +0700

    tampilan utama

---
+++ contoh-kode-program/halo-swing/src/main/java/com/muhardin/endy/belajar/swing/TampilanUtama.java
@@ -0,0 +1,40 @@
+package com.muhardin.endy.belajar.swing;
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class TampilanUtama extends JFrame {
+
+    public static void main(String[] xx){
+    
+        TampilanUtama t = new TampilanUtama();
+        t.setLocationRelativeTo(null);
+        t.setVisible(true);
+
+    }
+
+    public TampilanUtama(){
+    
+        setTitle("Aplikasi Saya");
+        setLayout(new FlowLayout());
+        pasangKomponen();
+        setSize(400,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    }
+
+    private void pasangKomponen(){
+        btnCari = new JButton("Cari");
+        txtMahasiswa = new JTextField(15);
+
+        getContentPane().add(new JLabel("Mahasiswa"));
+        getContentPane().add(txtMahasiswa);
+        getContentPane().add(btnCari);
+    }
+
+    private JButton btnCari;
+    private JTextField txtMahasiswa;
+}
